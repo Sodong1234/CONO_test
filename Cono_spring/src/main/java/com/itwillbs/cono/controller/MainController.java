e2e98758fc00473102ae59797cf7ae6575e2db4d
이소영 <lsy20830@naver.com>
2022-06-01 21:47:32 +0900

    프로젝트 오류 제거

--- Cono_spring/src/main/java/com/itwillbs/cono/controller/MainController.java
+++ Cono_spring/src/main/java/com/itwillbs/cono/controller/MainController.java
@@ -1,8 +1,40 @@
 package com.itwillbs.cono.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.itwillbs.cono.service.MainService;
+import com.itwillbs.cono.vo.MemberDTO;
+
 
 @Controller
 public class MainController {
-
+	
+	@Autowired
+	MainService service;
+	
+	@RequestMapping(value = "/MemberLogin.func", method = RequestMethod.GET)
+	public String login() {
+		return "member/login";
+	}
+	
+	@RequestMapping(value = "/MemberLoginPro.func", method = RequestMethod.POST)
+	public String login(@ModelAttribute MemberDTO member, Model model, HttpSession session) {
+		
+		MemberDTO memberResult = service.loginMember(member);
+		
+		if(memberResult == null) {
+			model.addAttribute("msg", "가입 실패!");
+			return "member/fail_back";
+		} else {
+			session.setAttribute("sId", memberResult.getMember_id());
+			return "redirect:/";
+		}
+	}
 }
