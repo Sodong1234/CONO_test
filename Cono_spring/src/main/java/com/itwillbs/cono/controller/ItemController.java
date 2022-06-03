1c3de5fe89495b0a29d3e11794df83178c1a493e
이소영 <lsy20830@naver.com>
2022-06-03 17:39:17 +0900

    상품 상세 페이지 진행중

--- Cono_spring/src/main/java/com/itwillbs/cono/controller/ItemController.java
+++ Cono_spring/src/main/java/com/itwillbs/cono/controller/ItemController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -80,7 +81,7 @@ public class ItemController {
 		
 	// ------------------ 상품 상세 정보 조회 (시작) - 이소영 ------------------
 		@RequestMapping(value = "/ItemDetail.shop", method = RequestMethod.GET)
-		public String selectItemDetail() {
+		public String selectItemDetail(String item_idx, Model model) {
 			
 			
 			
