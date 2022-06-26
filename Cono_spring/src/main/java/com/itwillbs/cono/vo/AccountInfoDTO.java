37b65c0e9b69eebf7bd471093b9255571ea8c6f8
HellChang <5tmdhkfem@naver.com>
2022-06-26 19:05:23 +0900

    계좌

---
+++ Cono_spring/src/main/java/com/itwillbs/cono/vo/AccountInfoDTO.java
@@ -0,0 +1,43 @@
+package com.itwillbs.cono.vo;
+
+public class AccountInfoDTO {
+	private String account_idx;
+	private String member_id;
+	private String accountName;
+	private String bankName;
+	private String accountNum;
+	
+	
+	public String getAccount_idx() {
+		return account_idx;
+	}
+	public void setAccount_idx(String account_idx) {
+		this.account_idx = account_idx;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	public String getAccountNum() {
+		return accountNum;
+	}
+	public void setAccountNum(String accountNum) {
+		this.accountNum = accountNum;
+	}
+	
+	
+}
